3a4b5790274e511cbd3836a5c372060988036b5a
ben <73725152+bentan1020@users.noreply.github.com>
2022-08-15 00:18:38 -0400

    awards and place details updated

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -15,12 +15,32 @@ const PlaceDetails = ({place}) => {
     <Card elevation={6}>
 
       <CardMedia 
-        style={{ height:350}} 
-        image={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
-        title={place.name} />
+          style={{ height:350}} 
+          image={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+          title={place.name} />
 
-      <CardContent>
-        <Typography gutterBottom variant="h5">{place.name}</Typography>
+        <CardContent>
+          <Typography gutterBottom variant="h5">{place.name}</Typography>
+
+          {/* price */}
+          <Box display="flex" justifyContent="space-between">
+              <Typography variant='subtitle1'>Price</Typography>
+              <Typography gutterBottom variant='subtitle1'>{place.price_level}</Typography>
+          </Box>
+
+          {/* ranking */}
+          <Box display="flex" justifyContent="space-between">
+              <Typography variant='subtitle1'>Ranking</Typography>
+              <Typography gutterBottom variant='subtitle1'>{place.ranking}</Typography>
+          </Box>
+
+          {place?.awards?.map((award) => (
+            <Box my={1} display="flex" justifyContent="space-between">
+              <img src={award.images.small} alt={award.display_name}></img>
+            </Box>
+          ))
+
+          }
       </CardContent>
         
     </Card>
